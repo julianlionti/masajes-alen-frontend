8405ff8803a748fe97678fad6ad793779a2d0362
Julián Lionti <julianlionti@hotmail.com>
2021-09-09 19:04:27 -0300

    cambiando a la ip

--- src/utils/Config.tsx
+++ src/utils/Config.tsx
@@ -1,8 +1,8 @@
 type NODE_ENV = "development" | "test" | "production";
 
-type ConfigProps = { NODE_ENV: NODE_ENV; USER_KEY: "MAUSER" };
+type ConfigProps = { NODE_ENV: NODE_ENV; USER_KEY: "MA_USER" };
 const Config: ConfigProps = {
   NODE_ENV: process.env.NODE_ENV as NODE_ENV,
-  USER_KEY: "MAUSER",
+  USER_KEY: "MA_USER",
 };
 export default Config;
